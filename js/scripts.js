014201eb542400b8615f25774c84b7f7a34b9abf
Aleksi Svaridze <aleksisvaridze@gmail.com>
2018-07-31 14:02:05 +0400

    Range Slider

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,6 @@
 $(document).ready(function(){
 
     // Header menu
-
-    
-
-
-
-
-
     $('#openHiddenSerchBox').on('click', function(){
         $(this).addClass('hiddenCloseImg');
         $('#closeHiddenSerchBox').removeClass('hiddenCloseImg');
@@ -43,7 +36,37 @@ $('.owl-category').owlCarousel({
     }
 });
 
-
-
-
 }); // .ready()
+
+var v_1 = document.getElementById('value-1');
+var v_2 = document.getElementById('value-2');
+var v_3 = document.getElementById('value-3');
+var v_4 = document.getElementById('value-4');
+
+
+var range = document.getElementById('range');
+
+function rangeValue() {
+    if ( range.value == 1 ) {
+        v_1.innerHTML = 1;
+    } else {
+        v_1.innerHTML = "";
+    };
+    if ( range.value == 2 ) {
+        v_2.innerHTML = 2;
+    } else {
+        v_2.innerHTML = "";
+    };
+    if ( range.value == 3 ) {
+        v_3.innerHTML = 3;
+    } else {
+        v_3.innerHTML = "";
+    };
+    if ( range.value == 4 ) {
+        v_4.innerHTML = 4;
+    } else {
+        v_4.innerHTML = "";
+    };
+};
+
+
